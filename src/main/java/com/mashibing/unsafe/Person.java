1405e63aac37b03a5f41c2e67b25994d003f612e
sjz66hs <sjz66hs@aliyun.com>
2020-09-17 13:48:37 +0800

    反射

---
+++ src/main/java/com/mashibing/unsafe/Person.java
@@ -0,0 +1,61 @@
+package com.mashibing.unsafe;
+
+import sun.misc.Unsafe;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+public class Person {
+    private int i=0;
+
+    private static  Unsafe UNSAFE;
+
+    private static long I_OFFSET;
+    static {
+        try {
+            //反射获取
+            Field field=Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            UNSAFE= (Unsafe) field.get(null);
+            I_OFFSET=UNSAFE.objectFieldOffset(Person.class.getDeclaredField("i"));
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+       Person person=new Person();
+
+        new Thread(()->{
+            System.out.println(I_OFFSET);
+            while (true){
+                boolean b = UNSAFE.compareAndSwapInt(person.i, I_OFFSET, person.i, person.i++);
+                if(b){
+                    System.out.println(person.i);
+                }else{
+                    System.out.println("false:"+person.i);
+                }
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        }).start();
+
+        new Thread(()->{
+            while (true){
+                boolean b = UNSAFE.compareAndSwapInt(person.i, I_OFFSET, person.i, person.i++);
+                if(b){
+                    System.out.println(person.i);
+                }else{
+                    System.out.println("false:"+person.i);
+                }
+            }
+        }).start();
+        System.in.read();
+
+    }
+
+}
